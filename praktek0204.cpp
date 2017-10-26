10dda7c1d6083e9d7a9a8da5941ffc6d5465041f
Ditaannisaftiarizky <dita.annisft.r@gmail.com>
2017-10-25 17:48:39 -0700

    perbaikan

--- praktek0204.cpp
+++ praktek0204.cpp
@@ -6,17 +6,17 @@ int main (){
 	double Luas_rumah = 42;
 	double Lantai_parket = 193*1215;
 	double Luas_1box = Lantai_parket*8/1000000;
-	double jml_box_paket, Biaya_parket, Biaya_pasang,
+	double Jml_box_parket, Biaya_parket, Biaya_pasang,
 	Totalbyr;
 	
 	cout << "Luas Rumah :" << Luas_rumah<<" m2 "<<endl;
 	cout << "Luas 1 box Parket :"
 	<< Luas_1box<< "m2"<<endl;
 	
-	jml_box_parket = Luas_rumah/Luas_1box;
-	cout<< "Jumlah Box Parket :" <<jml_box_parket<<endln;
+	Jml_box_parket = Luas_rumah/Luas_1box;
+	cout<< "Jumlah Box Parket :" <<Jml_box_parket<<endl;
 	
-	Biaya_parket = jml_box_parket*500;
+	Biaya_parket = Jml_box_parket*500;
 	cout << " Biaya Parket(Rp) :"<<Biaya_parket<<endl;
 	
 	Biaya_pasang = Luas_rumah*20;
