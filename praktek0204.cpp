e53de263f33c391d69f7261ac39100410d18f551
Ditaannisaftiarizky <dita.annisft.r@gmail.com>
2017-10-25 17:32:42 -0700

    masukan variabel

--- praktek0204.cpp
+++ praktek0204.cpp
@@ -3,4 +3,9 @@ using namespace std;
 
 int main (){
 	
+	double Luas_rumah = 42;
+	double Lantai_parket = 193*1215;
+	double Luas_1box = Lantai_parket*8/1000000;
+	double jml_box_paket, Biaya_parket, Biaya_pasang,
+	Totalbyr;
 }
